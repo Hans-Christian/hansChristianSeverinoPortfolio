85c8201fc7ae0d6092057f2e2d4d081c19d81d26
HansChristian <hc.sev15@gmail.com>
2021-01-07 18:30:29 -0500

    "Implemented suggestions that were given."

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 $(function(){
     const menuNav = function () {
         $menu = $(`.menu`);
+        $menuClick = $(`.menuClick`);
         $headerLinks = $(`.headerLinks`);
 
         $menu.on(`click`, function () {
             $headerLinks.toggleClass(`navActive`);
+
+            if ((($headerLinks).hasClass(`navActive`)) === true) {
+                $menuClick.text(`Click to Hide`);
+            } else {
+                $menuClick.text(`Click for Menu`);
+            }
         })
     }
 
